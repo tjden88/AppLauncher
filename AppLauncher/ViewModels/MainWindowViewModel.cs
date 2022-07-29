ac82cf44d6f52a536a22660ce33a5f2d4d3bf654
Денис (Home) <dultsevdv@gmail.com>
2022-07-30 04:55:07 +0500

    Расположение групп внутри окна

--- AppLauncher/ViewModels/MainWindowViewModel.cs
+++ AppLauncher/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,51 @@
-﻿using WPR.MVVM.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using AppLauncher.Views;
+using WPR.MVVM.ViewModels;
 
 namespace AppLauncher.ViewModels
 {
     internal class MainWindowViewModel : WindowViewModel
     {
-        
+
+        #region ColumnsCount : int - Количество колонок
+
+        /// <summary>Количество колонок</summary>
+        private int _ColumnsCount = 3;
+
+        /// <summary>Количество колонок</summary>
+        public int ColumnsCount
+        {
+            get => _ColumnsCount;
+            set => Set(ref _ColumnsCount, value);
+        }
+
+        #endregion
+
+
+        #region AppGroups : ObservableCollection<AppGroup> - Группы с ярлыками приложений
+
+        /// <summary>Группы с ярлыками приложений</summary>
+        private ObservableCollection<AppGroup> _AppGroups;
+
+        /// <summary>Группы с ярлыками приложений</summary>
+        public ObservableCollection<AppGroup> AppGroups
+        {
+            get => _AppGroups;
+            set => Set(ref _AppGroups, value);
+        }
+
+        #endregion
+
+
+        public IEnumerable<AppGroup> Groups1Column => AppGroups.Where(g => g.ColumnNumber == 1);
+        public IEnumerable<AppGroup> Groups2Column => AppGroups.Where(g => g.ColumnNumber == 2);
+        public IEnumerable<AppGroup> Groups3Column => AppGroups.Where(g => g.ColumnNumber == 3);
+        public IEnumerable<AppGroup> Groups4Column => AppGroups.Where(g => g.ColumnNumber == 4);
+        public IEnumerable<AppGroup> Groups5Column => AppGroups.Where(g => g.ColumnNumber == 5);
+
+
+
     }
 }
---
+++ AppLauncher/Views/AppGroup.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AppLauncher.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AppGroup.xaml
+    /// </summary>
+    public partial class AppGroup : UserControl
+    {
+        public AppGroup()
+        {
+            InitializeComponent();
+        }
+
+        #region ColumnNumber : int - Номер колонки, к которой привязана группа
+
+        /// <summary>Номер колонки, к которой привязана группа</summary>
+        public static readonly DependencyProperty ColumnNumberProperty =
+            DependencyProperty.Register(
+                nameof(ColumnNumber),
+                typeof(int),
+                typeof(AppGroup),
+                new PropertyMetadata(1));
+
+        /// <summary>Номер колонки, к которой привязана группа</summary>
+        [Category("AppGroup")]
+        [Description("Номер колонки, к которой привязана группа")]
+        public int ColumnNumber
+        {
+            get => (int) GetValue(ColumnNumberProperty);
+            set => SetValue(ColumnNumberProperty, value);
+        }
+
+        #endregion
+    }
+}
