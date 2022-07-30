f8514c14588f06a9421355979225441f74cb4e81
Денис (Home) <dultsevdv@gmail.com>
2022-07-31 02:42:41 +0500

    Методы работы с группами

--- AppLauncher/Services/DataManager.cs
+++ AppLauncher/Services/DataManager.cs
@@ -10,34 +10,73 @@ namespace AppLauncher.Services
     /// <summary>
     /// Загрузка и сохранение данных
     /// </summary>
-    public  class DataManager
+    public class DataManager
     {
         private readonly LinkService _LinkService;
         private readonly string _SettingsFileName = Path.Combine(Environment.CurrentDirectory, "Settings.json");
-
-        private class AppData
-        {
-            public List<AppLink> Links { get; set; } = new();
-
-            public List<Group> Groups { get; set; } = new();
-        }
-
 
         public DataManager(LinkService LinkService)
         {
             _LinkService = LinkService;
         }
 
+        private class AppData
+        {
+            public List<AppLink> Links { get; set; } = new();
+            public List<Group> Groups { get; set; } = new();
+        }
 
         private AppData _Data;
 
         private AppData Data => _Data ??= LoadData();
 
+        private AppData LoadData()
+        {
+            var data = DataSerializer.LoadFromFile<AppData>(_SettingsFileName);
+            return data ?? new AppData();
+        }
+
+        private void SaveData() => DataSerializer.SaveToFile(Data, _SettingsFileName);
+
+
+
+        #region Public
+
+        /// <summary>
+        /// Загрузить группы
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Group> LoadGroups() => Data.Groups.OrderBy(g => g.Id);
+
+        /// <summary>
+        /// Добавить группу
+        /// </summary>
+        /// <param name="Name">Имя группы</param>
+        /// <returns></returns>
+        public Group AddGroup(string Name)
+        {
+            var grName = string.IsNullOrEmpty(Name)
+                ? "Новая группа"
+                : Name;
+            var group = new Group()
+            {
+                Id = Data.Groups
+                    .Select(g => g.Id)
+                    .DefaultIfEmpty()
+                    .Max() + 1,
+                Name = grName,
+            };
+            Data.Groups.Add(group);
+            SaveData();
+            return group;
+        }
+
         /// <summary>
         /// Загрузить ярлыки группы
         /// </summary>
         /// <param name="GroupId">Id группы</param>
-        public IEnumerable<AppLink> LoadGroupLinks(int GroupId) => Data.Links.Where(l => l.GroupId == GroupId);
+        public IEnumerable<AppLink> LoadGroupLinks(int GroupId) =>
+            Data.Links.Where(l => l.GroupId == GroupId);
 
 
         /// <summary>
@@ -55,12 +94,6 @@ namespace AppLauncher.Services
         }
 
 
-        private AppData LoadData()
-        {
-            var data = DataSerializer.LoadFromFile<AppData>(_SettingsFileName);
-            return data ?? new AppData();
-        }
-
-        private void SaveData() => DataSerializer.SaveToFile(Data, _SettingsFileName);
+        #endregion
     }
 }
