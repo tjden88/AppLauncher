e483f84652a222d1e47dd5d42d7cb19be57e4b5e
Денис (Home) <dultsevdv@gmail.com>
2022-07-30 05:12:16 +0500

    Класс ярлыка

---
+++ AppLauncher/Models/AppLink.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace AppLauncher.Models
+{
+    /// <summary>
+    /// Ярлык приложения
+    /// </summary>
+    internal class AppLink
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+    }
+}
