f05aac71ef5a15c092a71754c0e1a7a850f4f36d
Денис (Home) <dultsevdv@gmail.com>
2022-07-30 05:27:34 +0500

    ViewModel ярлыка

--- AppLauncher/ViewModels/AppGroupViewModel.cs
+++ AppLauncher/ViewModels/AppGroupViewModel.cs
@@ -1,9 +1,11 @@
-﻿using WPR.MVVM.ViewModels;
+﻿using System.Collections.ObjectModel;
+using WPR.MVVM.ViewModels;
 
 namespace AppLauncher.ViewModels
 {
     public class AppGroupViewModel : ViewModel
     {
+
         #region ColumnNumber : int - Номер колонки этой группы
 
         /// <summary>Номер колонки этой группы</summary>
@@ -14,6 +16,20 @@ namespace AppLauncher.ViewModels
         {
             get => _ColumnNumber;
             set => Set(ref _ColumnNumber, value);
+        }
+
+        #endregion
+
+        #region Links : ObservableCollection<AppLinkViewModel> - Список ярлыков группы
+
+        /// <summary>Список ярлыков группы</summary>
+        private ObservableCollection<AppLinkViewModel> _Links;
+
+        /// <summary>Список ярлыков группы</summary>
+        public ObservableCollection<AppLinkViewModel> Links
+        {
+            get => _Links;
+            set => Set(ref _Links, value);
         }
 
         #endregion
---
+++ AppLauncher/ViewModels/AppLinkViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using WPR.MVVM.Commands;
+using WPR.MVVM.ViewModels;
+
+namespace AppLauncher.ViewModels
+{
+    /// <summary>
+    /// Модель - представление ярлыка для запуска
+    /// </summary>
+    public class AppLinkViewModel : ViewModel
+    {
+
+        #region Name : string - Имя ярлыка
+
+        /// <summary>Имя ярлыка</summary>
+        private string _Name;
+
+        /// <summary>Имя ярлыка</summary>
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        #endregion
+
+        #region Path : string - Путь для запуска
+
+        /// <summary>Путь для запуска</summary>
+        private string _Path;
+
+        /// <summary>Путь для запуска</summary>
+        public string Path
+        {
+            get => _Path;
+            set => Set(ref _Path, value);
+        }
+
+        #endregion
+
+        #region Command LaunchCommand - Запуск
+
+        /// <summary>Запуск</summary>
+        private Command _LaunchCommand;
+
+        /// <summary>Запуск</summary>
+        public Command LaunchCommand => _LaunchCommand
+            ??= new Command(OnLaunchCommandExecuted, CanLaunchCommandExecute, "Запуск");
+
+        /// <summary>Проверка возможности выполнения - Запуск</summary>
+        private bool CanLaunchCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Запуск</summary>
+        private void OnLaunchCommandExecuted()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Path,
+                UseShellExecute = true
+            });
+        }
+
+        #endregion
+    }
+}
