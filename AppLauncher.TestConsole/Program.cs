2f7510d28f1b18ca4b25aa3ec37bc42fa550fe82
Денис (Home) <dultsevdv@gmail.com>
2022-08-01 17:48:31 +0500

    Добавление тестового проекта

---
+++ AppLauncher.TestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+namespace AppLauncher.TestConsole
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] attr)
+        {
+        }
+
+    }
+
+}
