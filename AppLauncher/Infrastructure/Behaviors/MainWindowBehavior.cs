7fe52823f1478bd109b380fa4d5124273039057a
Денис (Home) <dultsevdv@gmail.com>
2022-08-02 06:31:54 +0500

    Анимация окна

--- AppLauncher/Infrastructure/Behaviors/MainWindowBehavior.cs
+++ AppLauncher/Infrastructure/Behaviors/MainWindowBehavior.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using AppLauncher.Views;
 using Microsoft.Xaml.Behaviors;
 
 namespace AppLauncher.Infrastructure.Behaviors
@@ -7,12 +11,55 @@ namespace AppLauncher.Infrastructure.Behaviors
     /// <summary>
     /// Поведение главного окна при изменении свойств
     /// </summary>
-    public class MainWindowBehavior : Behavior<Window>
+    public class MainWindowBehavior : Behavior<MainWindow>
     {
+        private readonly ScaleTransform _ScaleTransform = new();
+        private readonly CircleEase _CircleEase = new() { EasingMode = EasingMode.EaseIn };
+
+        private readonly Storyboard _StoryboardShow = new();
+        private readonly Storyboard _StoryboardHide = new();
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += OnLoad;
+        }
+
+        private void OnLoad(object Sender, RoutedEventArgs E)
+        {
+            AssociatedObject.RootGrid.RenderTransform = _ScaleTransform;
+            AssociatedObject.RootGrid.RenderTransformOrigin = new Point(0.5, 1);
+
+            var duration = TimeSpan.FromSeconds(0.3);
+
+            // Подготовка анимации
+            _StoryboardShow.Children.Add(new DoubleAnimation(0, 1, duration) { EasingFunction = _CircleEase });
+            _StoryboardShow.Children.Add(new DoubleAnimation(0, 1, duration) { EasingFunction = _CircleEase });
+
+            Storyboard.SetTargetProperty(_StoryboardShow.Children[0], new PropertyPath("RenderTransform.ScaleX"));
+            Storyboard.SetTargetProperty(_StoryboardShow.Children[1], new PropertyPath("RenderTransform.ScaleY"));
+            _StoryboardShow.Completed += (_, _) => AssociatedObject.EnableBlur();
 
 
 
 
+            _StoryboardHide.Children.Add(new DoubleAnimation(1, 0, duration) { EasingFunction = _CircleEase });
+            _StoryboardHide.Children.Add(new DoubleAnimation(1, 0, duration) { EasingFunction = _CircleEase });
+
+            Storyboard.SetTargetProperty(_StoryboardHide.Children[0], new PropertyPath("RenderTransform.ScaleX"));
+            Storyboard.SetTargetProperty(_StoryboardHide.Children[1], new PropertyPath("RenderTransform.ScaleY"));
+
+        }
+
+        private void Show()
+        {
+            _StoryboardShow.Begin(AssociatedObject.RootGrid);
+        }
+
+        private void Hide()
+        {
+            AssociatedObject.DisableBlur();
+            _StoryboardHide.Begin(AssociatedObject.RootGrid);
+        }
 
 
         #region IsHidden : bool - Скрыто ли окно
@@ -23,14 +70,25 @@ namespace AppLauncher.Infrastructure.Behaviors
                 nameof(IsHidden),
                 typeof(bool),
                 typeof(MainWindowBehavior),
-                new PropertyMetadata(default(bool)));
+                new PropertyMetadata(default(bool), OnHiddenChanged));
+
+        private static void OnHiddenChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        {
+            var behavior = (MainWindowBehavior)D;
+
+            if (E.NewValue is true)
+                behavior.Hide();
+            else
+                behavior.Show();
+
+        }
 
         /// <summary>Скрыто ли окно</summary>
         [Category("MainWindowBehavior")]
         [Description("Скрыто ли окно")]
         public bool IsHidden
         {
-            get => (bool) GetValue(IsHiddenProperty);
+            get => (bool)GetValue(IsHiddenProperty);
             set => SetValue(IsHiddenProperty, value);
         }
 
--- AppLauncher/Views/MainWindow.xaml.cs
+++ AppLauncher/Views/MainWindow.xaml.cs
@@ -11,11 +11,12 @@ namespace AppLauncher.Views
 
     internal enum AccentState
     {
-        ACCENT_DISABLED = 1,
-        ACCENT_ENABLE_GRADIENT = 0,
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
         ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
         ACCENT_ENABLE_BLURBEHIND = 3,
-        ACCENT_INVALID_STATE = 4
+        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+        ACCENT_INVALID_STATE = 5
     }
 
     [StructLayout(LayoutKind.Sequential)]
@@ -50,6 +51,69 @@ namespace AppLauncher.Views
         [DllImport("user32.dll")]
         internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
 
+        #region Window styles
+        [Flags]
+        public enum ExtendedWindowStyles
+        {
+            // ...
+            WS_EX_TOOLWINDOW = 0x00000080,
+            // ...
+        }
+
+        public enum GetWindowLongFields
+        {
+            // ...
+            GWL_EXSTYLE = (-20),
+            // ...
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            // Win32 SetWindowLong doesn't clear error on success
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else
+            {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+            {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
@@ -57,7 +121,17 @@ namespace AppLauncher.Views
         }
 
 
-        private void MainWindow_OnLoaded(object Sender, RoutedEventArgs E) => EnableBlur();
+        private void MainWindow_OnLoaded(object Sender, RoutedEventArgs E)
+        {
+            WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+
+            int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+
+            EnableBlur();
+        }
 
         internal void EnableBlur()
         {
@@ -71,7 +145,34 @@ namespace AppLauncher.Views
             var accentStructSize = Marshal.SizeOf(accent);
 
             var accentPtr = Marshal.AllocHGlobal(accentStructSize);
-            Marshal.StructureToPtr(accent, accentPtr, false);
+            Marshal.StructureToPtr(accent, accentPtr, true);
+
+            var data = new WindowCompositionAttributeData
+            {
+                Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+                SizeOfData = accentStructSize,
+                Data = accentPtr
+            };
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        internal void DisableBlur()
+        {
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy
+            {
+                AccentState = AccentState.ACCENT_DISABLED,
+                GradientColor = 25
+            };
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, true);
 
             var data = new WindowCompositionAttributeData
             {
