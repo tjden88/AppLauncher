f0f1ab67ec9938484b023e4d618c5292e04708dd
Денис (Home) <dultsevdv@gmail.com>
2022-08-03 01:09:06 +0500

    Оптимизации

--- AppLauncher/Infrastructure/Helpers/WindowPositionHelper.cs
+++ AppLauncher/Infrastructure/Helpers/WindowPositionHelper.cs
@@ -11,11 +11,16 @@ namespace AppLauncher.Infrastructure.Helpers
             if (mainWindow == null) return;
 
 
-            var screenWidth = SystemParameters.PrimaryScreenWidth;
-            var screenHeight = SystemParameters.PrimaryScreenHeight;
+            //var screenWidth = SystemParameters.PrimaryScreenWidth;
+            //var screenHeight = SystemParameters.PrimaryScreenHeight;
+
+
+            var screenWidth = SystemParameters.WorkArea.Width;
+            var screenHeight = SystemParameters.WorkArea.Height;
+
 
             var left = (screenWidth - mainWindow.Width) / 2;
-            var top = (screenHeight - mainWindow.Height) - 50;
+            var top = (screenHeight - mainWindow.Height) - 10;
 
             mainWindow.Left = left;
             mainWindow.Top = top;
