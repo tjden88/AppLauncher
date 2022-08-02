468f638fe858d439066ec8139262a2ceaf1e5bf1
Денис (Home) <dultsevdv@gmail.com>
2022-08-03 01:00:50 +0500

    Положение окна при развёртывании

--- AppLauncher/Infrastructure/Helpers/WindowPositionHelper.cs
+++ AppLauncher/Infrastructure/Helpers/WindowPositionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace AppLauncher.Infrastructure.Helpers
 {
@@ -7,8 +8,17 @@ namespace AppLauncher.Infrastructure.Helpers
         public static void SetMainWindowPosition()
         {
             var mainWindow = Application.Current.MainWindow;
-            mainWindow.Left = 0;
-            mainWindow.Top = 0;
+            if (mainWindow == null) return;
+
+
+            var screenWidth = SystemParameters.PrimaryScreenWidth;
+            var screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            var left = (screenWidth - mainWindow.Width) / 2;
+            var top = (screenHeight - mainWindow.Height) - 50;
+
+            mainWindow.Left = left;
+            mainWindow.Top = top;
         }
     }
 }
